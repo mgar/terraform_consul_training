18376196d4f2abf9a08b158f7c2e485373c02d47
Miguel Angel Garcia <mgarcia.inf@gmail.com>
2016-06-13 11:58:55 +0200

    dnsimple provider & new record
    Signed-off-by: Miguel Angel Garcia <mgarcia.inf@gmail.com>

--- dns.tf
+++ dns.tf
@@ -13,3 +13,16 @@
 #
 #     hashiconf-19ca14e7ea6328a42e0eb13d585e4c22
 #
+
+provider "dnsimple" {
+    email = "sethvargo+terraform@gmail.com"
+    token = "1Gam3SE2eIwZYtq70H5V5iAXiE9sGPmf"
+}
+
+resource "dnsimple_record" "example" {
+    domain = "terraform.rocks"
+    name = "terraform"
+    value = "${aws_instance.web.0.public_ip}"
+    type = "A"
+    ttl = 3600
+}

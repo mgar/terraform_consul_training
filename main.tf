a48d9c3544b1c204ae0170ee0fa456edd6fa5034
Miguel Angel Garcia <mgarcia.inf@gmail.com>
2016-06-13 10:54:19 +0200

    output from multiple resources, comma separated
    Signed-off-by: Miguel Angel Garcia <mgarcia.inf@gmail.com>

--- main.tf
+++ main.tf
@@ -54,9 +54,9 @@ resource "aws_instance" "web" {
 }
 
 output "public_ip" {
-    value = "${aws_instance.web.0.public_ip}"
+    value = "${join(", ", aws_instance.web.*.public_ip)}"
 }
 
 output "public_dns" {
-    value = "${aws_instance.web.0.public_dns}"
+    value = "${join(", ", aws_instance.web.*.public_dns)}"
 }
